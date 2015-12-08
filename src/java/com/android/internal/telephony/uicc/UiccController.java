cc62d3af3df8570b1bd3407969693ee6c040f7c7
Ethan Chen <intervigil@gmail.com>
2015-12-08 14:04:56 -0800

    Squash patches for bringing back registerForOn
    Revert "Use radio availability to sync card status instead of radio on"
    
     * Revert to AOSP behaviour for this. Introduce a few fixes later on
       which fix radio availability issues on both QC and legacy devices.
    
    This reverts commit af1cd8cbac506462e91d024f9c7c864d6002495b.
    
    Change-Id: Iae4063337d13eabc979acd172133d7744a270647
    
    UiccController: use registerForAvailable only for persist.radio.apm_sim_not_pwdn
    
    Some QCOM RILs allow the SIM to be loaded in airplane mode, thus
    CAF changed the callback to registerForAvailable, but that does
    not work on other RILs where the SIM information will not be
    loaded successfully.
    
    Change-Id: I9f9124417db2fe983e716a2825fc08d87511f8aa
    
    Use registerForAvailable when device is encrypted only for RIL version >= 9
    
    On older RIL versions, registerForAvailable causes the sim to not
    being detected. We fixed this for non-encrypted state, but missed it
    for encrypted state.
    
    Change-Id: I2a0fc670dc7318ee09a05d2ff8ebd54e03509e93

--- src/java/com/android/internal/telephony/uicc/UiccController.java
+++ src/java/com/android/internal/telephony/uicc/UiccController.java
@@ -121,16 +121,21 @@ public class UiccController extends Handler {
         if (DBG) log("Creating UiccController");
         mContext = c;
         mCis = ci;
+        boolean radioApmSimNotPwdn = SystemProperties.getBoolean(
+                "persist.radio.apm_sim_not_pwdn", false);
         for (int i = 0; i < mCis.length; i++) {
             Integer index = new Integer(i);
             mCis[i].registerForIccStatusChanged(this, EVENT_ICC_STATUS_CHANGED, index);
             // TODO remove this once modem correctly notifies the unsols
+
             // If the device has been decrypted or FBE is supported, read SIM when radio state is
             // available.
             // Else wait for radio to be on. This is needed for the scenario when SIM is locked --
             // to avoid overlap of CryptKeeper and SIM unlock screen.
-            if (DECRYPT_STATE.equals(SystemProperties.get("vold.decrypt")) ||
-                    StorageManager.isFileEncryptedNativeOrEmulated()) {
+            if ((DECRYPT_STATE.equals(SystemProperties.get("vold.decrypt")) &&
+                    mCis[i].getRilVersion() >= 9) || radioApmSimNotPwdn) {
+                // Reading ICC status in airplane mode is only supported in QCOM
+                // RILs when this property is set to true
                 mCis[i].registerForAvailable(this, EVENT_ICC_STATUS_CHANGED, index);
             } else {
                 mCis[i].registerForOn(this, EVENT_ICC_STATUS_CHANGED, index);
